245ba9cc267a470b4c36ef34cbdc9f9d55709eab
Listen丶 <c.cole@foxmail.com>
2019-02-19 15:14:44 +0800

    2019/2/19--2

--- src/main/java/com/listen/sspg/SspgApplication.java
+++ src/main/java/com/listen/sspg/SspgApplication.java
@@ -1,13 +1,13 @@
-package com.listen.sspg;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SspgApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SspgApplication.class, args);
-    }
-
-}
+//package com.listen.sspg;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SspgApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SspgApplication.class, args);
+//    }
+//
+//}
