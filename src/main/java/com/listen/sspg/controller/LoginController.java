c6d6f3fb1aefa479763e87949dcd9f410fe1b1f4
Listen丶 <c.cole@foxmail.com>
2019-02-27 10:57:34 +0800

    2019/2/27--log4j2

--- src/main/java/com/listen/sspg/controller/LoginController.java
+++ src/main/java/com/listen/sspg/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.listen.sspg.controller;
 import com.listen.sspg.basecore.ApiAcceptObj;
 import com.listen.sspg.basecore.ApiReturnObj;
 import com.listen.sspg.service.LoginServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     @Autowired
     private LoginServiceImpl loginService;
-
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @ResponseBody
     @RequestMapping("/doLogin")
     public ApiReturnObj<Object> doLogin(@Validated ApiAcceptObj apiAcceptObj){
-
+        logger.info("test");
         return loginService.doLogin(apiAcceptObj);
     }
 }
